84612ad9197274d94fc4c1357eaf9f950cd0cd1a
Mason Kim <qwerty3345@naver.com>
2023-04-11 18:39:47 +0900

    feat: 뒤 그리드 라인 구현 중
    - 아직 데이터가 완벽히 맞진 않아서 수정 필요

--- WeatherForecast/WeatherForecast/View/ForecastGraphView.swift
+++ WeatherForecast/WeatherForecast/View/ForecastGraphView.swift
@@ -14,11 +14,12 @@ class ForecastGraphView: UIView {
     private enum Constants {
         static let numberOfPresentingData = 8
 
-        static let margin: CGFloat = 20.0
-        static let topBorder: CGFloat = 60
-        static let bottomBorder: CGFloat = 50
+        static let horizontalMargin: CGFloat = 20
+        static let topMargin: CGFloat = 10
+        static let bottomMargin: CGFloat = 10
 
         static let circleDotDiameter: CGFloat = 8
+        static let gridStandard: CGFloat = 10
     }
 
     // MARK: - Properties
@@ -48,13 +49,14 @@ class ForecastGraphView: UIView {
     // MARK: - Draw
 
     override func draw(_ rect: CGRect) {
-        let humidities = forecastDatas.map { Double($0.weatherDetail.humidity) }
-        drawGraphLines(rect, of: humidities, color: .white)
+//        let humidities = forecastDatas.map { Double($0.weatherDetail.humidity) }
+//        drawGraphLines(rect, of: humidities, color: .white)
 
-        let minimumTemperatures = forecastDatas.map { $0.weatherDetail.minimumTemperature }
-        drawGraphLines(rect, of: minimumTemperatures, color: .blue)
-
+//        let minimumTemperatures = forecastDatas.map { $0.weatherDetail.minimumTemperature }
+//        drawGraphLines(rect, of: minimumTemperatures, color: .blue)
+//
         let maximumTemperatures = forecastDatas.map { $0.weatherDetail.maximumTemperature }
+        print(">>> maximumTemperatures", maximumTemperatures)
         drawGraphLines(rect, of: maximumTemperatures, color: .red.withAlphaComponent(0.5))
     }
 
@@ -67,7 +69,7 @@ class ForecastGraphView: UIView {
 
     // MARK: - Private
 
-    private func drawGraphLines(_ rect: CGRect, of graphPoints: [Double], color: UIColor) {
+    private func drawGraphLines(_ rect: CGRect, of pointValues: [Double], color: UIColor) {
         color.setFill()
         color.setStroke()
 
@@ -75,7 +77,7 @@ class ForecastGraphView: UIView {
 
         graphPath.lineWidth = 3
 
-        guard let points = drawingPoints(of: graphPoints, with: rect),
+        guard let points = drawingPoints(of: pointValues, with: rect),
               let firstPoint = points.first else { return }
 
         // 처음 포인트는 이동
@@ -89,31 +91,40 @@ class ForecastGraphView: UIView {
         graphPath.stroke()
 
         drawCircleDots(of: points)
+
+        drawGridLine(of: pointValues, with: rect)
     }
 
-    private func drawingPoints(of graphPoints: [Double], with rect: CGRect) -> [CGPoint]? {
+    private func drawingPoints(of pointValues: [Double], with rect: CGRect) -> [CGPoint]? {
         let width = bounds.width
         let height = bounds.height
 
-        let graphWidth = width - (Constants.margin * 2) - 4
+        let graphWidth = width - (Constants.horizontalMargin * 2) //- 4
         // 특정 점의 X 좌표
         let columnXPoint = { (column: Int) -> CGFloat in
             // 점들 사이에 공간을 계산함
-            let spacing = graphWidth / CGFloat(graphPoints.count - 1)
-            return CGFloat(column) * spacing + Constants.margin + 2
+            let spacing = graphWidth / CGFloat(pointValues.count - 1)
+            return CGFloat(column) * spacing + Constants.horizontalMargin //+ 2
         }
 
-        let graphHeight = height - Constants.topBorder - Constants.bottomBorder
-        guard let maxValue = graphPoints.max() else { return nil }  // 최대값의 y지점을 가장 상단으로 놓기 위해 (그래프 최고치의 기준점)
+        let graphHeight = height - (Constants.topMargin + Constants.bottomMargin)
+
+        let maxRange = maxRange(of: pointValues)
+        let minRange = minRange(of: pointValues)
+
+        print(">>> maxRange", maxRange)
+        print(">>> minRange", minRange)
 
         // 특정 점의 Y 좌표
         let columnYPoint = { (column: Int) -> CGFloat in
-            let graphPointValue = graphPoints[column]
-            let yPoint = CGFloat(graphPointValue) / CGFloat(maxValue) * graphHeight
-            return graphHeight + Constants.topBorder - yPoint // 아래가 0이고 커질수록 위로 올라가기에, - 로 뒤집어줌.
+            let graphPointValue = pointValues[column]
+            print(">>> graphPointValue", graphPointValue)
+            let yPoint = graphHeight * CGFloat(graphPointValue - minRange) / CGFloat(maxRange - minRange)
+            print(">>> yPoint", yPoint)
+            return graphHeight - yPoint + Constants.topMargin // + Constants.topBorder  // 아래가 0이고 커질수록 위로 올라가기에, - 로 뒤집어줌.
         }
 
-        return (0..<graphPoints.count).map { index in
+        return (0..<pointValues.count).map { index in
             CGPoint(x: columnXPoint(index), y: columnYPoint(index))
         }
     }
@@ -138,6 +149,43 @@ class ForecastGraphView: UIView {
         circles.forEach { circle in
             circle.fill()
         }
+    }
+
+    private func drawGridLine(of pointValues: [Double], with rect: CGRect) {
+        let width = bounds.width
+        let height = bounds.height
+        let graphHeight = height - (Constants.topMargin + Constants.bottomMargin)
+
+        UIColor.gray.setFill()
+        UIColor.gray.setStroke()
+
+        let graphPath = UIBezierPath()
+        graphPath.lineWidth = 1
+
+        let maxRange = maxRange(of: pointValues)
+        let minRange = minRange(of: pointValues)
+
+        let numberOfGrid = Int((maxRange - minRange) / Constants.gridStandard + 1)
+
+        let gridSplitValue = graphHeight / CGFloat(numberOfGrid)
+
+        (0..<numberOfGrid).forEach { gridCount in
+            let yPosition = gridSplitValue * CGFloat(gridCount) + Constants.topMargin
+            graphPath.move(to: CGPoint(x: 0, y: yPosition))
+            graphPath.addLine(to: CGPoint(x: width, y: yPosition))
+        }
+
+        graphPath.stroke()
+    }
+
+    private func minRange(of values: [Double]) -> Double {
+        let minumumValue = values.min() ?? 0
+        return (minumumValue / Constants.gridStandard).rounded() * Constants.gridStandard - Constants.gridStandard
+    }
+
+    private func maxRange(of values: [Double]) -> Double {
+        let maxValue = values.max() ?? 0.0
+        return (maxValue / Constants.gridStandard).rounded() * Constants.gridStandard + Constants.gridStandard
     }
 
 }
